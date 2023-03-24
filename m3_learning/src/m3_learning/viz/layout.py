cf59ffee56e76871a046122dbe1fa9c2ef20d33a
Josh Agar <jca318@lehigh.edu>
2023-03-24 12:05:43 -0400

    update with some changes that I will delete later

--- m3_learning/src/m3_learning/viz/layout.py
+++ m3_learning/src/m3_learning/viz/layout.py
@@ -3,9 +3,61 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import os
 import torch
+from matplotlib import (
+    pyplot as plt,
+    animation,
+    colors,
+    ticker,
+    path,
+    patches,
+    patheffects,
+)
+import string
+
+Path = path.Path
+PathPatch = patches.PathPatch
 
 
-def layout_fig(graph, mod=None, figsize = None):
+def path_maker(axes, locations, facecolor, edgecolor, linestyle, lineweight):
+    """
+    Adds path to figure
+    Parameters
+    ----------
+    axes : matplotlib axes
+        axes which to add the plot to
+    locations : numpy array
+        location to position the path
+    facecolor : str, optional
+        facecolor of the path
+    edgecolor : str, optional
+        edgecolor of the path
+    linestyle : str, optional
+        sets the style of the line, using conventional matplotlib styles
+    lineweight : float, optional
+        thickness of the line
+    """
+    vertices = []
+    codes = []
+    codes = [Path.MOVETO] + [Path.LINETO] * 3 + [Path.CLOSEPOLY]
+    # extracts the vertices used to construct the path
+    vertices = [
+        (locations[0], locations[2]),
+        (locations[1], locations[2]),
+        (locations[1], locations[3]),
+        (locations[0], locations[3]),
+        (0, 0),
+    ]
+    vertices = np.array(vertices, float)
+    #  makes a path from the vertices
+    path = Path(vertices, codes)
+    pathpatch = PathPatch(
+        path, facecolor=facecolor, edgecolor=edgecolor, ls=linestyle, lw=lineweight
+    )
+    # adds path to axes
+    axes.add_patch(pathpatch)
+
+
+def layout_fig(graph, mod=None, figsize=None):
 
     """Utility function that helps lay out many figures
 
@@ -18,8 +70,7 @@ def layout_fig(graph, mod=None, figsize = None):
     """
 
     # Sets the layout of graphs in matplotlib in a pretty way based on the number of plots
-    
-    
+
     if mod is None:
         # Select the number of columns to have in the graph
         if graph < 3:
@@ -34,15 +85,15 @@ def layout_fig(graph, mod=None, figsize = None):
             mod = 6
         elif graph < 37:
             mod = 7
-            
-    if figsize is None: 
+
+    if figsize is None:
         figsize = (3 * mod, 3 * (graph // mod + (graph % mod > 0)))
 
     # builds the figure based on the number of graphs and a selected number of columns
     fig, axes = plt.subplots(
         graph // mod + (graph % mod > 0),
         mod,
-        figsize= figsize,
+        figsize=figsize,
     )
 
     # deletes extra unneeded axes
@@ -218,3 +269,190 @@ def latent_generator(
             ax[j + embedding_small.shape[1]].set_ylim(y_scale)
             # ax[j + embedding_small.shape[1]].set_yticklabels('')
             plt.tight_layout(pad=1)
+
+
+def combine_lines(*args):
+
+    lines = []
+    labels = []
+
+    for arg in args:
+        # combine the two axes into a single legend
+        line, label = arg.get_legend_handles_labels()
+        lines += line
+        labels += label
+
+    return lines, labels
+
+
+def labelfigs(
+    axes, number, style="wb", loc="br", string_add="", size=14, text_pos="center"
+):
+    """Function that labels the figures
+
+    Args:
+        axes (axes object): axes to label
+        number (int): number value of the label in letters
+        style (str, optional): sets the style of the label. Defaults to 'wb'.
+        loc (str, optional): sets the location of the label. Defaults to 'br'.
+        string_add (str, optional): Adds a prefix string. Defaults to ''.
+        size (int, optional): sets the font size. Defaults to 14.
+        text_pos (str, optional): sets the position of the text. Defaults to 'center'.
+
+    Raises:
+        ValueError: The string provided for the style is not valid
+    """
+
+    # Sets up various color options
+    formatting_key = {
+        "wb": dict(color="w", linewidth=1.5),
+        "b": dict(color="k", linewidth=0),
+        "w": dict(color="w", linewidth=0),
+    }
+
+    # Stores the selected option
+    formatting = formatting_key[style]
+
+    # finds the position for the label
+    x_min, x_max = axes.get_xlim()
+    y_min, y_max = axes.get_ylim()
+    x_value = 0.08 * (x_max - x_min) + x_min
+
+    # Sets the location of the label on the figure
+    if loc == "br":
+        y_value = y_max - 0.1 * (y_max - y_min)
+        x_value = 0.08 * (x_max - x_min) + x_min
+    elif loc == "tr":
+        y_value = y_max - 0.9 * (y_max - y_min)
+        x_value = 0.08 * (x_max - x_min) + x_min
+    elif loc == "bl":
+        y_value = y_max - 0.1 * (y_max - y_min)
+        x_value = x_max - 0.08 * (x_max - x_min)
+    elif loc == "tl":
+        y_value = y_max - 0.9 * (y_max - y_min)
+        x_value = x_max - 0.08 * (x_max - x_min)
+    elif loc == "tm":
+        y_value = y_max - 0.9 * (y_max - y_min)
+        x_value = x_min + (x_max - x_min) / 2
+    elif loc == "bm":
+        y_value = y_max - 0.1 * (y_max - y_min)
+        x_value = x_min + (x_max - x_min) / 2
+    else:
+        raise ValueError(
+            "Unknown string format imported please look at code for acceptable positions"
+        )
+
+    # adds a custom string
+    if string_add == "":
+
+        # Turns to image number into a label
+        if number < 26:
+            axes.text(
+                x_value,
+                y_value,
+                string.ascii_lowercase[number],
+                size=14,
+                weight="bold",
+                ha=text_pos,
+                va="center",
+                color=formatting["color"],
+                path_effects=[
+                    patheffects.withStroke(
+                        linewidth=formatting["linewidth"], foreground="k"
+                    )
+                ],
+            )
+
+        # allows for double letter index
+        else:
+            axes.text(
+                x_value,
+                y_value,
+                string.ascii_lowercase[0] + string.ascii_lowercase[number - 26],
+                size=14,
+                weight="bold",
+                ha=text_pos,
+                va="center",
+                color=formatting["color"],
+                path_effects=[
+                    patheffects.withStroke(
+                        linewidth=formatting["linewidth"], foreground="k"
+                    )
+                ],
+            )
+    else:
+        # writes the text to the figure
+        axes.text(
+            x_value,
+            y_value,
+            string_add,
+            size=14,
+            weight="bold",
+            ha=text_pos,
+            va="center",
+            color=formatting["color"],
+            path_effects=[
+                patheffects.withStroke(
+                    linewidth=formatting["linewidth"], foreground="k"
+                )
+            ],
+        )
+
+
+def scalebar(axes, image_size, scale_size, units="nm", loc="br"):
+    """
+    Adds scalebar to figures
+    Parameters
+    ----------
+    axes : matplotlib axes
+        axes which to add the plot to
+    image_size : int
+        size of the image in nm
+    scale_size : str, optional
+        size of the scalebar in units of nm
+    units : str, optional
+        sets the units for the label
+    loc : str, optional
+        sets the location of the label
+    """
+
+    # gets the size of the image
+    x_lim, y_lim = axes.get_xlim(), axes.get_ylim()
+    x_size, y_size = np.abs(np.int(np.floor(x_lim[1] - x_lim[0]))), np.abs(
+        np.int(np.floor(y_lim[1] - y_lim[0]))
+    )
+    # computes the fraction of the image for the scalebar
+    fract = scale_size / image_size
+
+    x_point = np.linspace(x_lim[0], x_lim[1], np.int(np.floor(image_size)))
+    y_point = np.linspace(y_lim[0], y_lim[1], np.int(np.floor(image_size)))
+
+    # sets the location of the scalebar
+    if loc == "br":
+        x_start = x_point[np.int(0.9 * image_size // 1)]
+        x_end = x_point[np.int((0.9 - fract) * image_size // 1)]
+        y_start = y_point[np.int(0.1 * image_size // 1)]
+        y_end = y_point[np.int((0.1 + 0.025) * image_size // 1)]
+        y_label_height = y_point[np.int((0.1 + 0.075) * image_size // 1)]
+    elif loc == "tr":
+        x_start = x_point[np.int(0.9 * image_size // 1)]
+        x_end = x_point[np.int((0.9 - fract) * image_size // 1)]
+        y_start = y_point[np.int(0.9 * image_size // 1)]
+        y_end = y_point[np.int((0.9 - 0.025) * image_size // 1)]
+        y_label_height = y_point[np.int((0.9 - 0.075) * image_size // 1)]
+
+    # makes the path for the scalebar
+    path_maker(axes, [x_start, x_end, y_start, y_end], "w", "k", "-", 1)
+
+    # adds the text label for the scalebar
+    axes.text(
+        (x_start + x_end) / 2,
+        y_label_height,
+        "{0} {1}".format(scale_size, units),
+        size=14,
+        weight="bold",
+        ha="center",
+        va="center",
+        color="w",
+        path_effects=[patheffects.withStroke(linewidth=1.5, foreground="k")],
+    )
--- m3_learning/src/m3_learning/viz/style.py
+++ m3_learning/src/m3_learning/viz/style.py
@@ -20,9 +20,9 @@ def style(name = "default"):
             import matplotlib.pyplot as plt
             # setting default plotting params
             plt.rcParams['image.cmap'] = 'magma'
-            plt.rcParams['axes.labelsize'] = 16
-            plt.rcParams['xtick.labelsize'] = 14
-            plt.rcParams['ytick.labelsize'] = 14
+            plt.rcParams['axes.labelsize'] = 18
+            plt.rcParams['xtick.labelsize'] = 16
+            plt.rcParams['ytick.labelsize'] = 16
             plt.rcParams['figure.titlesize'] = 20
             plt.rcParams['xtick.direction'] = 'in'
             plt.rcParams['ytick.direction'] = 'in'
