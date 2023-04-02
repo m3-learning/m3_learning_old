ecd267cbf1f824373000841c04d5b23c64142bbc
Alibek Kaliyev <alk224@lehigh.edu>
2023-04-02 16:18:25 -0400

    speed measurement notebooks added

--- m3_learning/src/m3_learning/be/processing.py
+++ m3_learning/src/m3_learning/be/processing.py
@@ -6,6 +6,7 @@ Created on Sun Jan 24 16:34:00 2021
 import numpy as np
 import pyUSID as usid
 import h5py
+import torch
 import time
 from scipy import special
 import tensorflow as tf
@@ -331,6 +332,63 @@ def loop_fitting_function(type, V, y):
     else:
         print('No such parameters')
         return None
+    
+    
+def loop_fitting_function_torch(type, V, y):
+
+    if(type == '9 parameters'):
+        a0 = y[:, 0].type(torch.float64)
+        a1 = y[:, 1].type(torch.float64)
+        a2 = y[:, 2].type(torch.float64)
+        a3 = y[:, 3].type(torch.float64)
+        a4 = y[:, 4].type(torch.float64)
+        b0 = y[:, 5].type(torch.float64)
+        b1 = y[:, 6].type(torch.float64)
+        b2 = y[:, 7].type(torch.float64)
+        b3 = y[:, 8].type(torch.float64)
+        d = 1000
+        V1 = torch.tensor(V[:int(len(V) / 2)]).cuda()
+        V2 = torch.tensor(V[int(len(V) / 2):]).cuda()
+
+        g1 = (b1 - b0) / 2 * (torch.erf((V1 - a2) * d) + 1) + b0
+        g2 = (b3 - b2) / 2 * (torch.erf((V2 - a3) * d) + 1) + b2
+
+        y1 = (g1 * torch.erf((V1 - a2) / g1) + b0) / (b0 + b1)
+        y2 = (g2 * torch.erf((V2 - a3) / g2) + b2) / (b2 + b3)
+
+        f1 = a0 + a1 * y1 + a4 * V1
+        f2 = a0 + a1 * y2 + a4 * V2
+
+        loop_eval = torch.transpose(torch.cat((f1, f2), axis=0), 1, 0)
+        return loop_eval
+    elif(type == '13 parameters'):
+        a1 = y[:, 0].type(torch.float64)
+        a2 = y[:, 1].type(torch.float64)
+        a3 = y[:, 2].type(torch.float64)
+        b1 = y[:, 3].type(torch.float64)
+        b2 = y[:, 4].type(torch.float64)
+        b3 = y[:, 5].type(torch.float64)
+        b4 = y[:, 6].type(torch.float64)
+        b5 = y[:, 7].type(torch.float64)
+        b6 = y[:, 8].type(torch.float64)
+        b7 = y[:, 9].type(torch.float64)
+        b8 = y[:, 10].type(torch.float64)
+        Au = y[:, 11].type(torch.float64)
+        Al = y[:, 12].type(torch.float64)
+
+        # See supporting information for more information about the form of this function
+        S1 = ((b1 + b2) / 2) + ((b2 - b1) / 2) * torch.erf((V - b7) / b5)
+        S2 = ((b4 + b3) / 2) + ((b3 - b4) / 2) * torch.erf((V - b8) / b6)
+        Branch1 = (a1 + a2) / 2 + ((a2 - a1) / 2) * \
+            torch.erf((V - Au) / S1) + a3 * V
+        Branch2 = (a1 + a2) / 2 + ((a2 - a1) / 2) * \
+            torch.erf((V - Al) / S2) + a3 * V
+
+        return torch.squeeze(torch.cat((Branch1, torch.flipud(Branch2)), axis=0))
+    else:
+        print('No such parameters')
+        return None
+    
 
 def loop_fitting_function_tf(type, V, y):
     if(type == '9 parameters'):
