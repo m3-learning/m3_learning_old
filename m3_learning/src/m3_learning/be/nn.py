36d4997735ce83558e1ce97f4a83e259acab3089
jagar2 <jca92@drexel.edu>
2023-04-17 15:20:05 -0400

    update
    Former-commit-id: 50076ddf33fe2575267dc509966c85363e5be48f

--- m3_learning/src/m3_learning/be/nn.py
+++ m3_learning/src/m3_learning/be/nn.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 import torch
-from ..nn.SHO_fitter.SHO import SHO_fit_func_torch
 from ..optimizers.AdaHessian import AdaHessian
 from ..nn.random import random_seed
 from ..nn.benchmarks.inference import computeTime
@@ -10,6 +9,71 @@ import time
 import numpy as np
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+
+
+def SHO_fit_func_nn(params,
+                    wvec_freq,
+                    device='cpu'):
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
+    params = torch.tensor(params)
+
+    Amp = params[:, 0].type(torch.complex128)
+    w_0 = params[:, 1].type(torch.complex128)
+    Q = params[:, 2].type(torch.complex128)
+    phi = params[:, 3].type(torch.complex128)
+    wvec_freq = torch.tensor(wvec_freq)
+
+    Amp = torch.unsqueeze(Amp, 1)
+    w_0 = torch.unsqueeze(w_0, 1)
+    phi = torch.unsqueeze(phi, 1)
+    Q = torch.unsqueeze(Q, 1)
+
+    wvec_freq = wvec_freq.to(device)
+
+    numer = Amp * torch.exp((1.j) * phi) * torch.square(w_0)
+    den_1 = torch.square(wvec_freq)
+    den_2 = (1.j) * wvec_freq.to(device) * w_0 / Q
+    den_3 = torch.square(w_0)
+
+    den = den_1 - den_2 - den_3
+
+    func = numer / den
+
+    return func
+
+
+def SHO_fit_func_torch(parms,
+                       wvec_freq,
+                       device='cpu'):
+
+    Amp = parms[:, 0].type(torch.complex128)
+    w_0 = parms[:, 1].type(torch.complex128)
+    Q = parms[:, 2].type(torch.complex128)
+    phi = parms[:, 3].type(torch.complex128)
+    wvec_freq = torch.tensor(wvec_freq)
+
+    Amp = torch.unsqueeze(Amp, 1)
+    w_0 = torch.unsqueeze(w_0, 1)
+    phi = torch.unsqueeze(phi, 1)
+    Q = torch.unsqueeze(Q, 1)
+
+    wvec_freq = wvec_freq.to(device)
+
+    numer = Amp * torch.exp((1.j) * phi) * torch.square(w_0)
+    den_1 = torch.square(wvec_freq)
+    den_2 = (1.j) * wvec_freq.to(device) * w_0 / Q
+    den_3 = torch.square(w_0)
+
+    den = den_1 - den_2 - den_3
+
+    func = numer / den
+
+    return func
 
 
 class SHO_Model(nn.Module):
